9103fdf375ec4136616d09279fbcef4321968083
Suz4nGG <scxg240@gmail.com>
2022-10-08 15:35:56 -0500

    First commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import './styles/global.css'
+</script>
+
+<template>
+<h1 className="text-2xl font-bold underline">
+      Hello world!
+    </h1></template>
+
+<style scoped  lang="scss" >
+</style>
